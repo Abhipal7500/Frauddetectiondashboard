04ce02bda562bcba9691f5e01ef2108d143ed5f7
Abhishek Pal <131875068+Abhipal7500@users.noreply.github.com>
2025-06-03 15:08:31 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Welcome to the Fraud Detection Dashboard, By Abhishek PalS</h1>
-      <p className="text-gray-700 mb-6">Monitor and analyze fraudulent activities in real time, jgkjgkjlk.</p>
+      <p className="text-gray-700 mb-6">Monitor and analyze fraudulent activities in real time.</p>
       <button
         className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
         onClick={() => router.push("/login")}
